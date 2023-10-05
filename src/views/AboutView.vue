59e1c932329a1daeabd4b11a1927e63ee61ea79f
manar-m93 <supportweb6@zanobia.me>
2023-10-05 22:14:20 +0300

    initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Vue Todos</h1>
+    <br/>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi illum est dolore voluptas maiores facilis placeat sunt, ipsa, adipisci voluptates architecto nesciunt. Odit sed suscipit, minus possimus harum quis necessitatibus vitae iusto debitis velit placeat, eum molestias ipsam distinctio officia minima pariatur? Enim mollitia, tempora modi at molestiae nostrum deleniti totam unde nesciunt corporis porro a ducimus excepturi, natus vero assumenda voluptas veritatis, quibusdam nihil. Pariatur, reprehenderit nesciunt at facere tempora fugiat asperiores sunt error vitae distinctio doloribus architecto et culpa perspiciatis maiores, odio enim aut sequi non inventore obcaecati accusantium nisi incidunt veritatis? Autem architecto animi culpa cupiditate!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about{
+  margin: 30px 20px 20px !important;
+
+}
+</style>
---
+++ src/views/TodosHomeView.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { ref , watch, computed } from "vue";
+import { uid } from "uid";
+import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
+import { Icon } from '@iconify/vue';
+
+  components: { TodoCreator }
+  const todoList = ref([]);
+  watch(todoList , () => {
+    setTodoListLocalStorage ();
+  },{
+    deep:true
+  })
+  const todoCompleted = computed(()=>{
+    return todoList.value.every((todo)=>{
+      return todo.isCompleted 
+    })
+  })
+  const setTodoListLocalStorage =() =>{
+    localStorage.setItem('todoList',JSON.stringify(todoList.value))
+  }
+  const getTodoListLocalStorage =() =>{
+    const savedTodoList = JSON.parse(localStorage.getItem('todoList'))
+    if(savedTodoList){
+      todoList.value = savedTodoList
+    }
+  }
+  getTodoListLocalStorage();
+
+  const createTodo = (todo) =>{
+    todoList.value.push({id : uid(), todo , isCompleted : null ,isEditing :null});
+   }
+    const togglecomplete = (index) =>{
+      todoList.value[index].isCompleted = ! todoList.value[index].isCompleted ;
+    }
+    const toggleEditTodo = (index) =>{
+      todoList.value[index].isEditing = !   todoList.value[index].isEditing;
+    }
+    const updateTodo = (value,index) =>{
+      todoList.value[index].todo = value;
+    }
+    const deleteTodo = (id) =>{
+      todoList.value = todoList.value.filter(function(todo) {
+         return todo.id !== id
+         });
+    }
+  </script>
+ 
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <todo-creator @create-todo="createTodo"></todo-creator>
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <todo-item v-for="(todo ,index) in todoList"  :todo="todo" :index="index"
+       @toggle-complete="togglecomplete" 
+       @edit-todo="toggleEditTodo"
+       @update-todo="updateTodo" 
+       @delete-todo = "deleteTodo"/>
+    </ul>
+    <p class="todos-msg" v-else> 
+      <Icon icon="noto-v1:sad-but-relieved-face" color="#41b080" width="22" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
+    <p v-if="todoCompleted && todoList.length > 0" class="todos-msg"> 
+      <Icon icon="noto-v1:party-popper" color="#41b080" width="22" />
+      <span>You have completed all your todos!</span>
+    </p>
+  </main>
+</template>
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+</style>
